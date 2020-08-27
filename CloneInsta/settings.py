fb82f1cb44689a60b18666d35b42f5480ca54967
TK <wrtk007@gmail.com>
2020-08-27 10:58:11 +0900

    앱 만들기

--- CloneInsta/settings.py
+++ CloneInsta/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '48bov&4j#(*!u+&28rmf%q60us7^+s$7#rxp*k6z=2c1#m4sbf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pystagram',
+    'accounts'
 ]
 
 MIDDLEWARE = [
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CloneInsta
+
+참고 사이트 : https://chagokx2.tistory.com/30?category=871363
+
+1. [앱 만들기] ()
